77966f4d5852b4fe84ec6cbc9edbcaa433ff3e26
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2024-02-23 13:43:05 +0330

    DEV:021203:How to use function as function input using function address and pointers (callback function)

---
+++ FunctionPointerAsArgument.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool ascendingCompair(int number1, int number2)
+{
+  if (number1 > number2)
+    return true;
+  else
+    return false;
+}
+bool descendingCompair(int number1, int number2)
+{
+  if (number1 < number2)
+    return true;
+  else
+    return false;
+}
+
+void customSort(vector<int> numbers, bool (*sortFunction)(int, int))
+{
+
+  for (int i = 0; i < numbers.size(); i++)
+  {
+    for (int j = 0; j < numbers.size(); j++)
+    {
+      if (sortFunction(numbers[i], numbers[j]))
+      {
+        swap(numbers[i], numbers[j]);
+      }
+    }
+  }
+  for (int i = 0; i < numbers.size(); i++)
+  {
+    cout << numbers[i] << endl;
+  }
+}
+int main()
+{
+  vector<int> arrayOfIntegers = {77, 3, 4, 6, 32, 1};
+  bool (*sortFunc)(int, int) = descendingCompair;
+  customSort(arrayOfIntegers, sortFunc);
+  customSort(arrayOfIntegers, ascendingCompair);
+
+  return 0;
+}
