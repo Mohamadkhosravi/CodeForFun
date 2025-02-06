030e8353bdbeb648b3585ba49bdf0f9f492906fb
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2025-02-06 23:31:48 +0330

    Update : FactoryMethod with Static Cast

--- DesignPatterns/FactoryMethod /FactoryMethod.cpp
+++ DesignPatterns/FactoryMethod /FactoryMethod.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "string.h"
 
 #define PIN_LED_RED  2
 #define PIN_LED_BLUE 3
@@ -11,82 +9,71 @@
 class GPIO_Device {
 public:
     virtual void init() = 0;
-    virtual void on() ;
+    virtual void on() = 0;
     virtual void off() = 0;
     virtual ~GPIO_Device() {}
-
-    
 };
+
 class LED : public GPIO_Device {
 private:
     uint32_t pin;
-  
 public:
     LED(uint32_t gpio_pin) : pin(gpio_pin) {}
-   
-    void init() override {
-          pinMode((gpio_num_t)pin, GPIO_MODE_OUTPUT);     
+    void init() override { pinMode((gpio_num_t)pin, GPIO_MODE_OUTPUT); }
+    void on() override { digitalWrite((gpio_num_t)pin, HIGH); }
+    void off() override { digitalWrite((gpio_num_t)pin, LOW); }
+    void toggle(){ 
+        static bool state = false;
+        state = !state;
+        digitalWrite((gpio_num_t)pin, state);
     }
-    void on()  override { digitalWrite((gpio_num_t)pin,HIGH); }
-    void off() override{ digitalWrite((gpio_num_t)pin,LOW);  }
-    void toggle() { 
-      static bool state;
-      state=!state;
-      digitalWrite((gpio_num_t)pin,state);
-    }
-    
 };
-class buzzer : public GPIO_Device {
+
+class Buzzer : public GPIO_Device {
 private:
     uint32_t pin;
 public:
-    buzzer(uint32_t gpio_pin) : pin(gpio_pin) {}
-   
-    void init() override {
-          pinMode((gpio_num_t)pin, GPIO_MODE_OUTPUT);     
+    Buzzer(uint32_t gpio_pin) : pin(gpio_pin) {}
+    void init() override { pinMode((gpio_num_t)pin, GPIO_MODE_OUTPUT); }
+    void on() override { digitalWrite((gpio_num_t)pin, HIGH); }
+    void off() override { digitalWrite((gpio_num_t)pin, LOW); }
+    void bibTone(uint32_t delayMs) {
+        digitalWrite((gpio_num_t)pin, HIGH);
+        vTaskDelay(pdMS_TO_TICKS(delayMs));
+        digitalWrite((gpio_num_t)pin, LOW);
     }
-    void on()  override { digitalWrite((gpio_num_t)pin,HIGH); }
-    void off() override{ digitalWrite((gpio_num_t)pin,LOW);  }
-    void bibTone (uint32_t delayMs){
-
-      digitalWrite((gpio_num_t)pin,HIGH);
-      vTaskDelay(pdMS_TO_TICKS(delayMs));
-      digitalWrite((gpio_num_t)pin,LOW);
-      vTaskDelay(pdMS_TO_TICKS(delayMs));
-
-      }
 };
 
 class GPIO_Factory {
 public:
-    static GPIO_Device* createDevice( String type,int pin) {
+    static GPIO_Device* createDevice(const String& type, int pin) {
         if (type == "LED") {
-          return new LED(pin);
+            return new LED(pin);
         } else if (type == "Buzzer") {
-          return new buzzer(pin);
+            return new Buzzer(pin);
         }
-         return nullptr;
+        return nullptr;
     }
 };
+
 
 GPIO_Device* LEDRed = GPIO_Factory::createDevice("LED", PIN_LED_RED);
 GPIO_Device* LEDBlue = GPIO_Factory::createDevice("LED", PIN_LED_BLUE);
 GPIO_Device* buzzer1 = GPIO_Factory::createDevice("Buzzer", PIN_BUZZER);
+LED* ledRed = static_cast<LED*>(LEDRed);
 
 void setup() {
-   LEDRed->init();
-   LEDBlue->init();
-   buzzer1->init(); 
+    if (LEDRed) LEDRed->init();
+    if (LEDBlue) LEDBlue->init();
+    if (buzzer1) buzzer1->init();
 }
 
 void loop() {
-  LEDRed->on();
-  LEDBlue->off();
-  buzzer1->off();
-    LED* LEDRed = dynamic_cast<LED*>(LEDRed);
-    if (LEDRed) {
-        LEDRed->toggle(); 
-    } 
-
-
+   
+    if (ledRed) {// static cast is ok 
+        while (1) {
+            vTaskDelay(pdMS_TO_TICKS(500));
+            ledRed->toggle();
+        }
+    }
 }
