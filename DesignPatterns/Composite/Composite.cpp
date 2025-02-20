0996775259b85613136dc2b0876951491f09dfc8
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2025-02-20 23:33:44 +0330

    Update Composite .cpp

--- DesignPatterns/Composite/Composite.cpp
+++ DesignPatterns/Composite/Composite.cpp
@@ -1,92 +1,90 @@
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <cstdint> 
+
 using namespace std;
 
-typedef enum{
-NTC,
-RTD,
-INFRARED,
-DHT11,
-}
-tempSensor_t;
-class SensorCommpdnet{
-virtual int readValue()=0;
-virtual void showInformation()=0;
+typedef enum { I2C, SPI, UART } typeofComminaction_t;
+typedef enum { NTC, RTD, INFRARED, DHT11 } tempSensor_t;
 
-};
-int sensorRead(){
-return 23;
-}
-class TemperatureSensors : public SensorCommpdnet
-{
-private:
-tempSensor_t typeOfSensor;
-  int value;
+class SensorCommpdnet {
 public:
-    TemperatureSensors (tempSensor_t type):typeOfSensor(type){
-    };
-    int readValue() override{
-        value=sensorRead();
-    }
-    void showInformation() override{
-        cout<<"Type of Sensor is " << typeOfSensor<<"and value is :"<<value << endl;
-    }
-    ~TemperatureSensors ();
+    virtual int readValue() = 0;
+    virtual void showInformation() = 0;
+    virtual ~SensorCommpdnet() {} 
 };
 
-class NTC : public SensorCommpdnet{
-    private:
+int sensorRead() {
+    return 23;
+}
+
+class TemperatureSensors : public SensorCommpdnet {
+private:
+    tempSensor_t typeOfSensor;
+    int value;
+public:
+    TemperatureSensors(tempSensor_t type) : typeOfSensor(type) {}
+
+    int readValue() override {
+        value = sensorRead();
+        return value;
+    }
+
+    void showInformation() override {
+        cout << "Type of Sensor: " << typeOfSensor << " | Value: " << readValue() << endl;
+    }
+};
+
+class NTC : public SensorCommpdnet {
+private:
     uint8_t Pin;
-    public:
-    NTC(uint8_t pin):Pin(pin){};
-    int readValue() override{
-     //analogRead(pin)
-     return 25;
+public:
+    NTC(uint8_t pin) : Pin(pin) {}
+
+    int readValue() override {
+        return 25; 
     }
-    void showInformation() override{
-        cout<<"Type of Sensor is NTC and value is :"<<readValue()<< endl;
+
+    void showInformation() override {
+        cout << "Type: NTC | Value: " << readValue() << endl;
     }
-} ;
-class  RTD : public SensorCommpdnet{
-    private:
-    uint8_t Pin;
-    public:
-   RTD(uint8_t pin):Pin(pin){};
-    int readValue() override{
-     //analogRead(pin)
-     return 45;
-    }
-    void showInformation() override{
-        cout<<"Type of Sensor is RTD and value is :"<<readValue()<< endl;
-    }
-} ;
+};
 
 class SensorGroup : public SensorCommpdnet {
-    private:
-        std::vector<SensorCommpdnet*> sensors;   
-        public:
-        void addSensor(SensorCommpdnet* sensor) {
-            sensors.push_back(sensor);
-        }
-    
-        int readValue() override {
-            // Serial.println("Reading all sensors:");
-            for (auto & sensors : sensors) {
-            
-            
-            }
-        }
-        void showInformation() override {
-            for (auto & sensors : sensors) {
-                
-            }
-        }
-    };
+private:
+    vector<shared_ptr<SensorCommpdnet>> sensors;
+public:
+    void addSensor(shared_ptr<SensorCommpdnet> sensor) {
+        sensors.push_back(sensor);
+    }
 
-int main(int argc, char const *argv[])
-{   string FirstName=" ";
-    cout << "Enter your first name: ";
-    cin>>FirstName;
-    cout<<"\n ====================\n";
-    cout<<"Hello " << FirstName<<"!"<<endl;
+    int readValue() override {
+        int total = 0;
+        for (auto &sensor : sensors) {
+            total += sensor->readValue();
+        }
+        return total;
+    }
+
+    void showInformation() override {
+        for (auto &sensor : sensors) {
+            sensor->showInformation();
+        }
+    }
+};
+
+int main() {
+    auto sensorGroup = make_shared<SensorGroup>();
+
+    auto ntcSensor = make_shared<NTC>(7);
+    auto tempSensor = make_shared<TemperatureSensors>(NTC);
+
+    sensorGroup->addSensor(ntcSensor);
+    sensorGroup->addSensor(tempSensor);
+
+    cout << "Sensor Group Data:\n";
+    sensorGroup->showInformation();
+
     return 0;
 }
