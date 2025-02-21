a8fc959d476072cb3a9e1431984018197964221a
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2025-02-22 01:51:52 +0330

    Update Composite .C

--- DesignPatterns/Composite/Composite.c
+++ DesignPatterns/Composite/Composite.c
@@ -12,7 +12,9 @@ void action2() {
 void action3() {
     printf("Run Action3\n");
 }
- 
+void action4() {
+    printf("Run Action4\n");
+} 
 
 void Item(char *title, void (*action)()) {
     printf("->%s", title);
@@ -25,28 +27,51 @@ typedef struct  {
     
 } MenuComponent;
 
+
+
  MenuComponent *createMenuItem(char *title, void(*action)(void)){
 
-    MenuComponent *Menu= (MenuComponent *)malloc(sizeof(MenuComponent));
-    Menu->TITLE=title;
-    Menu->ACTION=action;
-    return Menu;
+    MenuComponent *Item= (MenuComponent *)malloc(sizeof(MenuComponent));
+    Item->TITLE=title;
+    Item->ACTION=action;
+    return Item;
 
 }
+
+
+
 MenuComponent *Menu1Item1;
 MenuComponent *Menu1Item2;
 MenuComponent *Menu1Item3;
 MenuComponent *Menu1Item4;
 
+MenuComponent * createSubMenu(MenuComponent *Item){
+    static uint32_t CounterOfChilde =1;
+
+      MenuComponent *subMenu;
+      subMenu=(MenuComponent *) realloc(subMenu,sizeof(MenuComponent) );
+      return subMenu;
+
+}
+
  int main() {
  
-    
+    MenuComponent *subMenu;
     Menu1Item1=createMenuItem("item1",action1);
     Menu1Item2=createMenuItem("item2",action2);
-    Menu1Item3=createMenuItem("item1",action1);
-    Menu1Item4=createMenuItem("item2",action2);
-    Menu1Item1->ACTION();
-    Menu1Item2->ACTION();
-    Menu1Item3->ACTION();
+    Menu1Item3=createMenuItem("item1",action3);
+    Menu1Item4=createMenuItem("item2",action4);
+    
+    subMenu=createSubMenu(Menu1Item1);
+    subMenu=createSubMenu(Menu1Item2);
+    subMenu=createSubMenu(Menu1Item3);
+    subMenu=createSubMenu(Menu1Item4);
+
+     subMenu->ACTION();
+     
+    // subMenu[2]->ACTION();
+    // subMenu[3]->ACTION();
+    // subMenu[4]->ACTION();
+
     return 0;
 }
