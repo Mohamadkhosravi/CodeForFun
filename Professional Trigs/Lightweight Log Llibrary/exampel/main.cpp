ba4a237fa273e16c92e2141a97a0da9b003c05dc
Mohammad <khosravimmd@gmail.com>
2025-04-23 15:53:08 +0330

    Add files via upload
    + Professional Trigs , using Lightweight Log Llibrary

---
+++ Professional Trigs/Lightweight Log Llibrary/exampel/main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include<Loger.h>
+void motor_init() ;
+void motor_check_temp(int temp);
+int main() {
+    motor_init();
+    motor_check_temp(85); 
+    return 0;
+}
+
+void motor_init() {
+    LOG_INFO("motor init done");
+}
+
+void motor_check_temp(int temp) {
+    if (temp > 80)
+        LOG_WARN("temp high: %d", temp);
+    if (temp > 100)
+        LOG_ERROR("motor overheating!");
+}
